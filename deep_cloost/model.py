4345cb2c988f785b1b7cc4a442f22434b1f7611f
Alex Nichol <unixpickle@gmail.com>
2020-01-12 09:20:47 -0500

    draft encoder model

---
+++ deep_cloost/model.py
@@ -0,0 +1,41 @@
+import torch
+import torch.nn as nn
+
+
+class Encoder(nn.Module):
+    def __init__(self, shape, options, base, loss_fn, output_fn=None, num_layers=0):
+        self.shape = shape
+        self.options = options
+        self.base = base
+        self.loss_fn = loss_fn
+        self.output_layers = []
+        self.output_biases = []
+        for i in range(num_layers):
+            layer = output_fn()
+            bias = nn.Parameter(torch.zeros((options,) + shape))
+            self.output_layers.append(layer)
+            self.output_biases.append(bias)
+            self.add_module('output%d' % i, layer)
+            self.add_module('bias%d' % i, bias)
+
+    def forward(self, inputs):
+        """
+        Apply the encoder and track the corresponding
+        reconstructions.
+
+        Returns:
+          A tuple (encodings, reconstructions)
+        """
+        base_out = self.base(inputs)
+        current_outputs = inputs
+        encodings = []
+        for i, (layer, bias) in enumerate(zip(self.output_layers, self.output_biases)):
+            layer_out = bias + layer(base_out)
+            new_outputs = current_outputs[:, None] + layer_out
+            losses = torch.stack([torch.stack([self.loss_fn(new_outputs[i, j])
+                                               for j in range(new_outputs.shape[1])])
+                                  for i in range(new_outputs.shape[0])])
+            indices = torch.argmin(losses, dim=-1)
+            encodings.append(indices)
+            current_outputs = new_outputs[:, indices]
+        return torch.stack(encodings, dim=-1), current_outputs
