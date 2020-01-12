23e0fd3e00ffd6c094c0085e7039b46e165a41c3
Alex Nichol <unixpickle@gmail.com>
2020-01-12 15:21:37 -0500

    cloost seems to work

--- deep_cloost/model.py
+++ deep_cloost/model.py
@@ -3,20 +3,18 @@ import torch.nn as nn
 
 
 class Encoder(nn.Module):
-    def __init__(self, shape, options, base, loss_fn, output_fn=None, num_layers=0):
+    def __init__(self, shape, options, base, loss_fn, output_fn=None, num_stages=0):
+        super().__init__()
         self.shape = shape
         self.options = options
         self.base = base
         self.loss_fn = loss_fn
         self.output_layers = []
         self.output_biases = []
-        for i in range(num_layers):
+        for _ in range(num_stages):
             layer = output_fn()
             bias = nn.Parameter(torch.zeros((options,) + shape))
-            self.output_layers.append(layer)
-            self.output_biases.append(bias)
-            self.add_module('output%d' % i, layer)
-            self.add_module('bias%d' % i, bias)
+            self.add_stage(layer, bias)
 
     def forward(self, inputs):
         """
@@ -28,35 +26,48 @@ class Encoder(nn.Module):
         """
         current_outputs = torch.zeros_like(inputs)
         encodings = []
-        for i, (layer, bias) in enumerate(zip(self.output_layers, self.output_biases)):
-            base_out = self.base(current_outputs)
-            layer_out = bias + layer(base_out)
-            new_outputs = current_outputs[:, None] + layer_out
+        for i in range(self.num_stages):
+            new_outputs = self.apply_stage(i, current_outputs)
             losses = torch.stack([torch.stack([self.loss_fn(new_outputs[i, j], inputs[i])
                                                for j in range(new_outputs.shape[1])])
                                   for i in range(new_outputs.shape[0])])
-            indices = torch.argmin(losses, dim=-1)
+            indices = torch.argmin(losses, dim=1)
             encodings.append(indices)
-            current_outputs = new_outputs[:, indices]
+            current_outputs = new_outputs[range(new_outputs.shape[0]), indices]
+        if len(encodings) == 0:
+            return torch.zeros((inputs.shape[0], 0), dtype=torch.long), current_outputs
         return torch.stack(encodings, dim=-1), current_outputs
 
     def decode(self, codes):
-        current_outputs = torch.zeros((codes.shape[0],) + self.shape, device=self.device)
-        for i, (layer, bias) in enumerate(zip(self.output_layers, self.output_biases)):
-            base_out = self.base(current_outputs)
-            layer_out = bias + layer(base_out)
-            new_outputs = current_outputs[:, None] + layer_out
-            current_outputs = new_outputs[:, codes[i]]
+        current_outputs = torch.zeros((codes.shape[0],) + self.shape, device=codes.device)
+        for i in range(self.num_stages):
+            new_outputs = self.apply_stage(i, current_outputs)
+            current_outputs = new_outputs[:, codes[:, i]]
         return current_outputs
 
     def reconstruct(self, inputs, batch=None):
         if batch is None:
             return self(inputs)[1]
         results = []
-        for i in range(0, inputs.shape, batch):
+        for i in range(0, inputs.shape[0], batch):
             results.append(self(inputs[i:i+batch])[1])
         return torch.cat(results, dim=0)
 
+    def apply_stage(self, idx, x):
+        layer = self.output_layers[idx]
+        bias = self.output_biases[idx]
+        base_out = self.base(x)
+        layer_out = layer(base_out)
+        layer_out = bias + layer_out
+        return x[:, None] + layer_out
+
+    def add_stage(self, layer, bias):
+        i = self.num_stages
+        self.output_layers.append(layer)
+        self.output_biases.append(bias)
+        self.add_module('output%d' % i, layer)
+        self.register_parameter('bias%d' % i, bias)
+
     @property
-    def device(self):
-        return next(self.parameters()).device
+    def num_stages(self):
+        return len(self.output_layers)
--- deep_cloost/train.py
+++ deep_cloost/train.py
@@ -1,4 +1,4 @@
-from scipi.cluster.vq import kmeans2
+from scipy.cluster.vq import kmeans2
 import torch
 import torch.nn as nn
 
@@ -6,12 +6,13 @@ import torch.nn as nn
 def initialize_biases(encoder, data, batch=None):
     with torch.no_grad():
         recon = encoder.reconstruct(data, batch=batch)
-    recon = recon.requires_grad_(True)
+    recon = recon.detach().requires_grad_(True)
     loss = encoder.loss_fn(recon, data)
     grads, = torch.autograd.grad(loss, recon)
+    grads /= torch.std(grads)
     dense_grads = grads.view(grads.shape[0], -1)
     clusters, _ = kmeans2(dense_grads.cpu().numpy(), encoder.options, minit='++')
-    torch_clusters = torch.from_numpy(clusters).to(encoder.device)
+    torch_clusters = -torch.from_numpy(clusters).to(data.device)
     # TODO: line search to find optimal scales for each
     # cluster center.
     return nn.Parameter(torch_clusters.view(-1, *grads.shape[1:]))
