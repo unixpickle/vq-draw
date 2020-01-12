a8017c3157e05e9f10bd16eac7148e7d2ffb8ccc
Alex Nichol <unixpickle@gmail.com>
2020-01-12 11:51:42 -0500

    use kmeans++

--- deep_cloost/train.py
+++ deep_cloost/train.py
@@ -1,4 +1,4 @@
-from scipi.cluster.vq import kmeans
+from scipi.cluster.vq import kmeans2
 import torch
 import torch.nn as nn
 
@@ -9,5 +9,5 @@ def initialize_biases(encoder, data, batch=None):
     recon = recon.requires_grad_(True)
     loss = encoder.loss_fn(recon, data)
     grads, = torch.autograd.grad(loss, recon)
-    clusters, _ = kmeans(grads.cpu().numpy(), encoder.options)
+    clusters, _ = kmeans2(grads.cpu().numpy(), encoder.options, minit='++')
     return nn.Parameter(torch.from_numpy(clusters).to(encoder.device))
