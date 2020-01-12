9f35df955f2d9fef6ad8aa322852255990d3596d
Alex Nichol <unixpickle@gmail.com>
2020-01-12 15:32:05 -0500

    line search for init

--- deep_cloost/train.py
+++ deep_cloost/train.py
@@ -1,4 +1,5 @@
 from scipy.cluster.vq import kmeans2
+from scipy.optimize import minimize_scalar
 import torch
 import torch.nn as nn
 
@@ -11,8 +12,27 @@ def initialize_biases(encoder, data, batch=None):
     grads, = torch.autograd.grad(loss, recon)
     grads /= torch.std(grads)
     dense_grads = grads.view(grads.shape[0], -1)
-    clusters, _ = kmeans2(dense_grads.cpu().numpy(), encoder.options, minit='++')
-    torch_clusters = -torch.from_numpy(clusters).to(data.device)
-    # TODO: line search to find optimal scales for each
-    # cluster center.
-    return nn.Parameter(torch_clusters.view(-1, *grads.shape[1:]))
+    clusters, labels = kmeans2(dense_grads.cpu().numpy(), encoder.options, minit='++')
+
+    biases = -torch.from_numpy(clusters).to(data.device)
+    biases = biases.view(-1, *grads.shape[1:])
+    biases = _scale_line_search(recon, data, encoder.loss_fn, biases, labels)
+
+    return nn.Parameter(biases)
+
+
+def _scale_line_search(recon, data, loss, biases, labels):
+    results = []
+    for i in range(biases.shape[0]):
+        indices = [j for j, x in enumerate(labels) if x == i]
+        sub_recon = recon[indices]
+        sub_data = data[indices]
+        bias = biases[i]
+
+        def loss_fn(alpha):
+            new_recon = sub_recon + bias * alpha
+            new_loss = loss(new_recon, sub_data)
+            return new_loss.item()
+        alpha = float(minimize_scalar(loss_fn, bracket=(0, 1000)).x)
+        results.append(bias * alpha)
+    return torch.stack(results)
