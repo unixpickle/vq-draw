2cbf7f4b44d1521d60eb66dfc0de70bbc1592934
Alex Nichol <unixpickle@gmail.com>
2020-01-19 16:14:09 -0500

    loss base class

--- deep_cloost/losses.py
+++ deep_cloost/losses.py
@@ -1,10 +1,33 @@
+from abc import abstractmethod
+
 import torch
 import torch.nn as nn
 
 
-class MSELoss(nn.Module):
-    def loss_grid(self, x, y):
-        return torch.mean(torch.pow(x - y, 2), dim=tuple(range(2, len(x.shape))))
+class LossFunc(nn.Module):
+    """
+    A loss function that can be applied in a 2-D batch.
+    """
 
+    @abstractmethod
+    def loss_grid(self, output, target):
+        """
+        Compute the losses for a target batch, given a set
+        of potential predictions.
+
+        Args:
+            output: an [N x C x ...] Tensor.
+            target: an [N x 1 x ...] Tensor.
+
+        Returns:
+            An [N x C] Tensor of losses.
+        """
+        pass
+
+
+class MSELoss(LossFunc):
     def forward(self, x, y):
         return torch.mean(torch.pow(x - y, 2))
+
+    def loss_grid(self, x, y):
+        return torch.mean(torch.pow(x - y, 2), dim=tuple(range(2, len(x.shape))))
